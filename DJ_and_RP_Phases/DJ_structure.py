7472f9ce95648046a0a1cb51eb2fc6bac15227a8
jenny42 <changchongning@gmail.com>
2020-11-07 22:16:03 -0800

    PEP8 style

--- DJ_and_RP_Phases/DJ_structure.py
+++ DJ_and_RP_Phases/DJ_structure.py
@@ -1,6 +1,6 @@
 import sys
 
-# this script create POSCAR of Dion-Jacobson phases that have the general formula M+1A(n-1)BnO(3n+1).
+# this script creates POSCAR of Dion-Jacobson phases that have the general formula M+1A(n-1)BnO(3n+1).
 # usage python3 DJ_structure.py M A B O n
 
 M, A, B, O = sys.argv[1:5]
@@ -17,36 +17,36 @@ a = 4.0
 
 # number of elements
 elem = {
-        M: 1,
-        A: (n-1),
-        B: n,
-        O: (3*n+1),
-        }
+    M: 1,
+    A: (n-1),
+    B: n,
+    O: (3*n+1),
+}
 
-fname = '%s%s%d%s%d%s%d_DJ_POSCAR_unit.vasp'%(M, A, n-1, B, n, O, 3*n+1)
+fname = '%s%s%d%s%d%s%d_DJ_POSCAR_unit.vasp' % (M, A, n-1, B, n, O, 3*n+1)
 with open(fname, 'w') as f:
-    f.write('%s\n'%fname)
+    f.write('%s\n' % fname)
     f.write('1.0\n')
-    f.write('%18.10f %18.10f %18.10f\n'%(a, 0, 0))
-    f.write('%18.10f %18.10f %18.10f\n'%(0, a, 0))
-    f.write('%18.10f %18.10f %18.10f\n'%(0, 0, (n+0.5*k)*a))
-    f.write('%5s %5s %5s %5s\n'%(M, A, B, O))
-    f.write('%5d %5d %5d %5d\n'%(elem[M], elem[A], elem[B], elem[O]))
+    f.write('%18.10f %18.10f %18.10f\n' % (a, 0, 0))
+    f.write('%18.10f %18.10f %18.10f\n' % (0, a, 0))
+    f.write('%18.10f %18.10f %18.10f\n' % (0, 0, (n+0.5*k)*a))
+    f.write('%5s %5s %5s %5s\n' % (M, A, B, O))
+    f.write('%5d %5d %5d %5d\n' % (elem[M], elem[A], elem[B], elem[O]))
     f.write('Direct\n')
     # M
-    f.write('%18.10f %18.10f %18.10f\n'%(0.5, 0.5, 0.5))
+    f.write('%18.10f %18.10f %18.10f\n' % (0.5, 0.5, 0.5))
     # A
     for i in range(n-1):
-        f.write('%18.10f %18.10f %18.10f\n'%(0.5, 0.5, (-n+2+2*i)*d ))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.5, 0.5, (-n+2+2*i)*d))
 
     # B
     for i in range(n):
-        f.write('%18.10f %18.10f %18.10f\n'%(0.0, 0.0, (1-n+2*i)*d  ))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.0, 0.0, (1-n+2*i)*d))
 
     # O
     for i in range(n):
-        f.write('%18.10f %18.10f %18.10f\n'%(0.0, 0.5 , (1-n+2*i)*d  ))
-        f.write('%18.10f %18.10f %18.10f\n'%(0.5, 0.0 , (1-n+2*i)*d  ))
-    
+        f.write('%18.10f %18.10f %18.10f\n' % (0.0, 0.5, (1-n+2*i)*d))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.5, 0.0, (1-n+2*i)*d))
+
     for i in range(n+1):
-        f.write('%18.10f %18.10f %18.10f\n'%(0.0, 0.0, (-n+2*i)*d ))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.0, 0.0, (-n+2*i)*d))
--- DJ_and_RP_Phases/RP_structure.py
+++ DJ_and_RP_Phases/RP_structure.py
@@ -1,6 +1,6 @@
 import sys
 
-# this script create POSCAR of Ruddleson-Popper phases that have the general formula A(n+1)BnO(3n+1).
+# this script creates POSCAR of Ruddleson-Popper phases that have the general formula A(n+1)BnO(3n+1).
 # usage python3 RP_structure.py A B O n
 
 A, B, O = sys.argv[1:4]
@@ -17,38 +17,38 @@ a = 4.0
 
 # number of elements
 elem = {
-        A: 2*(n+1),
-        B: 2*n,
-        O: 2*(3*n+1),
-        }
+    A: 2*(n+1),
+    B: 2*n,
+    O: 2*(3*n+1),
+}
 
-fname = '%s%d%s%d%s%d_RP_POSCAR_unit.vasp'%(A, n+1, B, n, O, 3*n+1)
+fname = '%s%d%s%d%s%d_RP_POSCAR_unit.vasp' % (A, n+1, B, n, O, 3*n+1)
 with open(fname, 'w') as f:
-    f.write('%s\n'%fname)
+    f.write('%s\n' % fname)
     f.write('1.0\n')
-    f.write('%18.10f %18.10f %18.10f\n'%(a, 0, 0))
-    f.write('%18.10f %18.10f %18.10f\n'%(0, a, 0))
-    f.write('%18.10f %18.10f %18.10f\n'%(0, 0, (2*n+k)*a))
-    f.write('%5s %5s %5s\n'%(A, B, O))
-    f.write('%5d %5d %5d\n'%(elem[A], elem[B], elem[O]))
+    f.write('%18.10f %18.10f %18.10f\n' % (a, 0, 0))
+    f.write('%18.10f %18.10f %18.10f\n' % (0, a, 0))
+    f.write('%18.10f %18.10f %18.10f\n' % (0, 0, (2*n+k)*a))
+    f.write('%5s %5s %5s\n' % (A, B, O))
+    f.write('%5d %5d %5d\n' % (elem[A], elem[B], elem[O]))
     f.write('Direct\n')
     # A
     for i in range(n+1):
-        f.write('%18.10f %18.10f %18.10f\n'%(0.5, 0.5, (-n+2*i)*d ))
-        f.write('%18.10f %18.10f %18.10f\n'%(0.0, 0.0 , (n+2*i)*d+l))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.5, 0.5, (-n+2*i)*d))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.0, 0.0, (n+2*i)*d+l))
 
     # B
     for i in range(n):
-        f.write('%18.10f %18.10f %18.10f\n'%(0.0, 0.0 , (1-n+2*i)*d  ))
-        f.write('%18.10f %18.10f %18.10f\n'%(0.5, 0.5, (n+1+2*i)*d+l))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.0, 0.0, (1-n+2*i)*d))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.5, 0.5, (n+1+2*i)*d+l))
 
     # O
     for i in range(n):
-        f.write('%18.10f %18.10f %18.10f\n'%(0.0, 0.5, (1-n+2*i)*d  ))
-        f.write('%18.10f %18.10f %18.10f\n'%(0.5, 0.0 , (1-n+2*i)*d  ))
-        f.write('%18.10f %18.10f %18.10f\n'%(0.0, 0.5, (n+1+2*i)*d+l))
-        f.write('%18.10f %18.10f %18.10f\n'%(0.5, 0.0 , (n+1+2*i)*d+l))
-    
+        f.write('%18.10f %18.10f %18.10f\n' % (0.0, 0.5, (1-n+2*i)*d))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.5, 0.0, (1-n+2*i)*d))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.0, 0.5, (n+1+2*i)*d+l))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.5, 0.0, (n+1+2*i)*d+l))
+
     for i in range(n+1):
-        f.write('%18.10f %18.10f %18.10f\n'%(0.0, 0.0 , (-n+2*i)*d ))
-        f.write('%18.10f %18.10f %18.10f\n'%(0.5, 0.5, (n+2*i)*d+l))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.0, 0.0, (-n+2*i)*d))
+        f.write('%18.10f %18.10f %18.10f\n' % (0.5, 0.5, (n+2*i)*d+l))
